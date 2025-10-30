a37dbf8a0a65ac5aa53e37c63d907875d07dffd4
moborn <mobo987@aucklanduni.ac.nz>
2025-10-30 21:56:44 +1300

    rfid working

---
+++ Hansel & Gretel/gingerbreadhouse/src/main.cpp
@@ -0,0 +1,63 @@
+
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+
+int relay_pin = 3;
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  // Serial.println("Approximate your card to the reader...");
+  // Serial.println();
+  pinMode(relay_pin, OUTPUT);
+  digitalWrite(relay_pin, HIGH);
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "23 6C E3 C7") //change here the UID of the card/cards that you want to give access
+  {
+   
+    delay(3000); //wait 3 sec to ensure is in place
+    if (content.substring(1) == "23 6C E3 C7") {
+       Serial.println("Authorized access");
+       Serial.println();
+       digitalWrite(relay_pin, LOW); //activate the relay
+    }
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    // delay(3000);
+  }
+} 
