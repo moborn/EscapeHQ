c39b242c25b3a28b157d74a66a25aadb3e1a337d
moborn <mobo987@aucklanduni.ac.nz>
2026-01-07 21:50:36 +1300

    spi confusion

--- Hansel & Gretel/gingerbreadhouse/src/main.cpp
+++ Hansel & Gretel/gingerbreadhouse/src/main.cpp
@@ -1,3 +1,4 @@
+#define MFRC522_SPICLOCK (1000000u) // Sets SPI bus to 4MHz
 
 #include <SPI.h>
 #include <MFRC522.h>
@@ -13,8 +14,12 @@ void setup()
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
+  //print init details
+  
   // Serial.println("Approximate your card to the reader...");
   // Serial.println();
+  mfrc522.PCD_DumpVersionToSerial();  
+
   pinMode(relay_pin, OUTPUT);
   digitalWrite(relay_pin, HIGH);
 
@@ -58,6 +63,8 @@ void loop()
  
  else   {
     Serial.println(" Access denied");
+    //print content substring
+    Serial.println(content.substring(1));
     // delay(3000);
   }
 } 
